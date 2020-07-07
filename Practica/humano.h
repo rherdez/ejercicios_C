f2202678ca3a285ab5862878b2ce1bef279af77a
tito82 <roberto.herna@gmail.com>
2020-07-07 14:40:35 -0600

    practica clases

---
+++ Practica/humano.h
@@ -0,0 +1,13 @@
+#ifndef HUMANO_H
+#define HUMANO_H
+#include <iostream>
+using namespace std;
+class humano
+{
+	public:
+		int id;
+		string nombre;
+	protected:
+};
+
+#endif
---
+++ Practica/practica_clase.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "humano.h"
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+humano h;
+int main(int argc, char** argv) {
+	h.id=5;
+	h.nombre="Pedro";
+	cout<<h.nombre<<endl;
+	cout<<h.id<<endl;
+	return 0;
+}

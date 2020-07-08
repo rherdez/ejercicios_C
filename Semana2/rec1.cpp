318f680bdea274b86d60f12201ee02e81f22cca8
tito82 <roberto.herna@gmail.com>
2020-07-08 17:51:57 -0600

    $m

--- Semana2/rec1.cpp
+++ Semana2/rec1.cpp
@@ -1,7 +1,45 @@
 #include <iostream>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
+using namespace std;
+int sumatoria(int);
+int exponente (int a, int b);
+double factorial(double);
 int main(int argc, char** argv) {
+	cout<<"La sumatoria es: "<<sumatoria(5)<<endl;
+	cout<<"Exponente: "<<exponente(2,3)<<endl;
+	cout<<"El Factorial es: "<<factorial(50)<<endl;
+	
 	return 0;
 }
+int sumatoria(int x){
+	if(x==0){
+		return x;
+	}
+	else{
+		return sumatoria(x-1)+x;
+	}
+		
+}
+
+int exponente (int a, int b) {
+	if (b == 0) 
+		{return 1;}
+	else 
+	{return exponente(a,b-1)*a;}
+}
+
+
+
+
+double factorial (double x) {
+	if (x == 1) {
+		return 1;
+	}		
+	else {
+		return factorial(x-1)*x;
+	}
+		
+}
+
+

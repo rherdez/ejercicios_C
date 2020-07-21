0de537b666e9e4b9b7e591e799f2fb7744a9b517
tito82 <roberto.herna@gmail.com>
2020-07-20 19:00:15 -0600

    $m

--- Semana3/lista_simple.cpp
+++ Semana3/lista_simple.cpp
@@ -5,10 +5,13 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 using namespace std;
 
-nodo *I,*F,*T;
+nodo *I,*F,*T,*A;
 nodo temp;
 void agregar();
 void presentar();
+void buscar(int);
+void registro();
+void eliminar();
 int op;
 int main(int argc, char** argv) {
 	
@@ -16,13 +19,16 @@ int main(int argc, char** argv) {
 		cout<<"Menu"<<endl;
 		cout<<"1) Agregar"<<endl;
 		cout<<"2) Presentar"<<endl;
+		cout<<"3) Buscar"<<endl;
+		cout<<"4) Modificar"<<endl;
+		cout<<"5) Eliminar"<<endl;
 		cout<<"0) Salir"<<endl;
 		cin>>op;
 		
 		switch(op){
 			case 1:
 				cout<<"Ingrese un numero: "<<endl;
-				cin>>temp.n;
+				cin>>temp.id;
 				cout<<"Ingrese un nombre: "<<endl;
 				cin>>temp.nombre;
 				cout<<"Ingrese sueldo: "<<endl;
@@ -33,6 +39,40 @@ int main(int argc, char** argv) {
 			case 2:
 				presentar();
 				break;
+				
+			case 3:
+					cout<<"Ingrese el ID a Buscar: "<<endl;
+					cin>>temp.id;
+					buscar(temp.id);
+					if(T!=NULL){
+						cout<<"Registro Encontrado"<<endl;
+						registro();
+					}
+			case 4:
+					cout<<"Ingrese el ID a Modificar: "<<endl;
+					cin>>temp.id;
+					buscar(temp.id);
+					if(T!=NULL){
+						registro();
+						cout<<"Ingrese los nuevos valores:"<<endl;
+						cout<<"==================================="<<endl;
+						cout<<"Ingrese un nombre: "<<endl;
+						cin>>T->nombre;
+						cout<<"Ingrese sueldo: "<<endl;
+						cin>>T->sueldo;																		
+					}
+				case 5:
+					cout<<"Ingrese el ID a Buscar: "<<endl;
+					cin>>temp.id;
+					buscar(temp.id);
+					if(T!=NULL){
+						cout<<"Registro Eliminado"<<endl;
+						registro();
+						eliminar();
+					}
+					
+					
+					
 			case 0:
 				break;
 			default:
@@ -74,7 +114,52 @@ void presentar(){
 	
 	while(T!=NULL){
 		cout<<"ID: "<<T->id<<endl;
+		cout<<"Nombre: "<<T->nombre<<endl;
+		cout<<"Sueldo: "<<T->sueldo<<endl;
+		cout<<"Deduccion: "<<T->deduccion()<<endl;
+		cout<<"Sueldo Neto: "<<T->sueldo-T->deduccion()<<endl;
 		T=T->sig;
+	}
+	
+}
+void buscar(int codigo){
+		bool encontrado=false;
+		T=I;
+		A=T;
+	
+	while(T!=NULL && !encontrado){
+		if(T->id==codigo){
+			encontrado=true;			
+		}
+		else{
+			A=T;
+			T=T->sig;
+		}
+		
+	}
+	
+	if(!encontrado){
+		cout<<"Este codigo no existe en nuestra Lista"<<endl;
+	}		
+}
+
+void registro(){
+	cout<<"ID: "<<T->id<<endl;
+		cout<<"Nombre: "<<T->nombre<<endl;
+		cout<<"Sueldo: "<<T->sueldo<<endl;
+		cout<<"Deduccion: "<<T->deduccion()<<endl;
+		cout<<"Sueldo Neto: "<<T->sueldo-T->deduccion()<<endl;	
+}
+void eliminar(){
+	if(T==I){
+		I=T->sig;
+	}
+	else if(T==F){
+		F=A;
+		A->sig=NULL;
+	}
+	else{
+		A->sig=T->sig;
 	}
 	
 }

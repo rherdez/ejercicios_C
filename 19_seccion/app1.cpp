4d8c62def5243499d74d6d11337869a2c17381d2
tito82 <roberto.herna@gmail.com>
2020-09-17 19:43:42 -0600

    $m

--- 19_seccion/app1.cpp
+++ 19_seccion/app1.cpp
@@ -3,19 +3,6 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 using namespace std;
 int main(int argc, char** argv) {
-	int n1,n2,n3;
-	/*bool
-	double
-	float
-	char
-	long*/
-	
-	cout<<"Ingrese N1"<<endl;
-	cin>>n1;
-	cout<<"Ingrese N2 \n";
-	cin>>n2;
-	n3=n1+n2;
-	cout<<"El resultado es: "<<n3<<"\n";
-	
+	cout<<"Hola Mundo"<<endl;
 	return 0;
 }

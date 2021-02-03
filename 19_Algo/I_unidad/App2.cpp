1cf637a4ae8aa0d22794adb1c2afbd825862f6a9
herdezclase <herdez.clase@gmail.com>
2021-02-02 20:40:03 -0600

    $m

---
+++ 19_Algo/I_unidad/App2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+int main(int argc, char** argv) {
+
+	double x=5;
+    cout<<"La sumatoria es: "<<(x/2.0)*(x+1)<<endl;
+   
+	return 0;
+}
---
+++ 19_Algo/I_unidad/app1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std;
+int main(int argc, char** argv) {
+	char letra;
+	
+	cout<<"Ingrese una letra"<<endl;
+	cin>>letra;
+	
+		switch(letra){
+			case 'a': 
+			case 'e':	
+			case 'i':		
+			case 'o':			
+			case 'u':
+				cout<<"Vocal"<<endl;
+				break;
+			default:
+				cout<<"Consonante"<<endl;
+		}
+							
+	return 0;
+}
